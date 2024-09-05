c15bdd17ef6722633a0fe6c13ebf07e1f7a270e8
VincentB00 <vincentbui1999@gmail.com>
2024-09-05 13:26:25 -0400

    init

---
+++ src/main/java/com/viescloud/llc/bubble/BubbleApplication.java
@@ -0,0 +1,13 @@
+package com.viescloud.llc.bubble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbleApplication.class, args);
+	}
+
+}
