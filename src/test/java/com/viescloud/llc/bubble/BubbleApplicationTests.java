ac3638a426a229270d0f6cd85467bd2e786b5811
VincentB00 <vincentbui1999@gmail.com>
2024-09-05 13:43:37 -0400

    remove test

--- src/test/java/com/viescloud/llc/bubble/BubbleApplicationTests.java
+++ src/test/java/com/viescloud/llc/bubble/BubbleApplicationTests.java
@@ -1,13 +1,5 @@
 package com.viescloud.llc.bubble;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BubbleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
